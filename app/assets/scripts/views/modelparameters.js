779c2845c8c9ef37526c0f0cbcd06323fc513d01
sawojcicki <sawojcicki@gmail.com>
2017-03-17 17:36:28 -0400

    Update modelparameters.js

--- app/assets/scripts/views/modelparameters.js
+++ app/assets/scripts/views/modelparameters.js
@@ -203,6 +203,7 @@ var ModelParameters = Backbone.View.extend({
         values: yearValues,
         density: 10,
         format: wNumb({
+            toFixed(yearValues, 0),
         }),
         stepped: true
       }
